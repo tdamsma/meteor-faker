afbe1bbffb46b280f842fb6d92c1a992cc86e8bf
digilord <grae@digilord.net>
2013-08-15 15:12:37 -0700

    Package creation

---
+++ package.js
@@ -0,0 +1,9 @@
+Package.describe({
+    summary: "Faker.js packaged for Meteor. Generate massive amounts of fake data"
+});
+
+Npm.depends({Faker: "0.5.11"});
+
+Package.on_use(function (api) {
+    api.add_files('.npm/node_modules/Faker/MinFaker.js', 'client');
+});
