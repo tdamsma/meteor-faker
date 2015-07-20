b44799477160a52117ebe4d18a6569c5b42b8a17
Gabriel Rubens <grsabreu@gmail.com>
2015-07-20 15:09:31 -0300

    Updating digilord:faker version info

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     summary: "Faker.js packaged for Meteor. Generate massive amounts of fake data",
     name: "digilord:faker",
-    version: "1.0.5",
+    version: "1.0.6",
     git: "https://github.com/digilord/meteor-faker.git"
 });
 
